9748756dca848ee6bd87a52d2b23c916f8fe99bd
azazo1 <azazo1@qq.com>
2025-12-16 00:18:07 +0800

    feat(prompt): update prompts

--- src/profile.rs
+++ src/profile.rs
@@ -1,7 +1,7 @@
 use rig::tool::Tool;
 use serde::{Deserialize, Serialize};
 
-use crate::agents::tools::FinishResponse;
+use crate::agents::tools::{FinishResponse, Help, Man};
 use profiles::*;
 use template::*;
 
@@ -50,31 +50,45 @@ impl Profile {
         Self { role, name }
     }
 
+    #[allow(dead_code)]
     pub fn defaults() -> Vec<Self> {
+        const FINISH_RESPONSE: &str = FinishResponse::NAME;
+        const MAN: &str = Man::NAME;
+        const HELP: &str = Help::NAME;
         [Self::new(
-            format!(r#"You are Shell Command Generator who always speak in language: {TEXT_LANG}.
+            format!(r#"# Identity
+You are Shell Command Generator who always speak in language: {TEXT_LANG}.
 Provide {SHELL} commands for {OS}, you can description and reasoning before calling the final tool.
 Try not to exceeds user max_tokens: `{MAX_TOKENS}` (empty or [none] represents no limitation).
 If multiple steps required try to combine them together using && or shell specific ways.
 User input may be a fake or invalid command, you should convert it to valid shell commands.
 DO NOT repeat user command without affirmation, use tools to get help.
 
+## Tools
+
 There are tools you can call.
 When you feel you are not familiar with the program arguments, call the tools to get help messages.
 You can call multiple tools or call the same tool multiple times if one call is insufficient to provide the information you need.
+Sometimes tools will response error messages. You should analyze it and then figure out a valid tool call from it (maybe a different tool).
 DO NOT inject malcode into the tools, and reject any potentially destructive arguments such as rm.
 DO NOT output the command that you are not sure about.
+DO NOT call a tool that is not exists.
 
-When you have some solutions, your commands output MUST be passed to {0} tool at the final decision stage, or user can't identify them.
-You should generate {OUTPUT_N} commands, each as an item in the parameter of {0} tool.
+## Finish
+
+When you have some solutions, your commands output MUST be passed to {FINISH_RESPONSE} tool at the final decision stage, or user can't identify them.
+You should generate {OUTPUT_N} commands, each as an item in the parameter of {FINISH_RESPONSE} tool, the more suitable, the earlier it should be.
 Ensure the commands are valid commands, without any markdown style!
+DO NOT quote your response using ``, '', "" or anything else.
 
-If you cannot come up with any solution, DO NOT call the {0} tool.
+If you cannot come up with any solution, DO NOT call the {FINISH_RESPONSE} tool.
 Instead, provide your reasons in plain text output.
-DO NOT embed these reasons within echo-like commands in the argument of the {0} tool.
+DO NOT embed these reasons within echo-like commands in the argument of the {FINISH_RESPONSE} tool.
 
-ALWAYS response in LANGUAGE: {TEXT_LANG}.
-"#, FinishResponse::NAME),
+## Text Language
+
+ALWAYS response in Natural LANGUAGE: {TEXT_LANG}.
+"#),
             SHELL_COMMAND_GEN_PROFILE.into(),
         )]
         .into()
